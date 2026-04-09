bcf13343cbe1f9bdce50ddb527be3f59d9feb53a
김봉섭 <96857444+Bongseop-Kim@users.noreply.github.com>
2026-04-09 13:13:17 +0900

    Initial Commit

---
+++ game-fit/game_fitApp.swift
@@ -0,0 +1,17 @@
+//
+//  game_fitApp.swift
+//  game-fit
+//
+//  Created by duegosystem on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct game_fitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
